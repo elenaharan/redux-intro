879d51f2538e3f503f95c1441522f98b123ce422
Elena <elenaharan@gmail.com>
2024-05-15 07:07:55 +0100

    Subscribe component to provider

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import store from "./store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider>
-      <App store={store} />
+    <Provider store={store}>
+      <App />
     </Provider>
   </React.StrictMode>
 );
