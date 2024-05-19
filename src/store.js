a0a358ddf7d30b6ed61bf486cf49fec5d23ef9c1
Elena <elenaharan@gmail.com>
2024-05-19 10:51:42 +0100

    add composeWithDevTools hook

--- src/store.js
+++ src/store.js
@@ -2,12 +2,16 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const routeReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
 
-const store = createStore(routeReducer, applyMiddleware(thunk));
+const store = createStore(
+  routeReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
